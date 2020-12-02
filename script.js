75d9cfb108ddcd71a0da430ec33d5cf10ab825dc
Oleksii Savchenko <savchenko.orient@gmail.com>
2020-12-02 22:30:21 +0200

    Игра готова

---
+++ script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+let isNumber = function (n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+};
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+
+function guessTheNumber() {
+  
+  let number = +prompt('Угадай число от 1 до 100.');
+  
+  if (number > randomNumber) {
+    alert('Загаданное число меньше');
+    guessTheNumber();
+  } if (number < randomNumber) {
+  alert('Загаданноое число больше');
+  guessTheNumber();
+  } if (!isNumber(number)) {
+    alert('Введите число!');
+    guessTheNumber();
+  } if (number === false) {
+    alert('Игра окончена');
+    
+  } if (number === randomNumber) {
+    alert('Поздравляю, вы угадали!');
+  }
+  return number;
+}
+
+guessTheNumber();
+
